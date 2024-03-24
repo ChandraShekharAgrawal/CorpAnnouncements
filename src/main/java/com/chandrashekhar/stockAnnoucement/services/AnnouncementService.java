e641279a31f73912b8adf3e93d6acddfa2fd4688
Chandra Shekhar <chandrashekhar@Shivanis-MacBook-Air.local>
2024-03-24 16:44:57 +0530

    changing to selenium

--- src/main/java/com/chandrashekhar/stockAnnoucement/services/AnnouncementService.java
+++ src/main/java/com/chandrashekhar/stockAnnoucement/services/AnnouncementService.java
@@ -1,9 +1,8 @@
 package com.chandrashekhar.stockAnnoucement.services;
 
-import java.io.IOException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,13 +10,9 @@ public class AnnouncementService {
     String url = "https://www.bseindia.com/corporates/ann.html";
 
     public String scrapAnnouncements(){
-        try{
-            Document document = Jsoup.connect(url).get();
-            return document.title();
-        }
-        catch (IOException e){
-            return ("error message is: "+ e);
-        }
+        WebDriver driver = new ChromeDriver();
+        driver.get(url);
+        return driver.getTitle();
     }   
     
 }
